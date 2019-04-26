899244598a4415285f52fe90ed6bf5a7fbb91752
Laurie Roy <laurieroy.dev@gmail.com>
2019-04-25 19:24:21 -0700

    connect components to store

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,21 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 
 class App extends Component {
   render() {
+    console.log('this', this);
     return (
       <div>React App</div>
     );
   }
 }
 
-export default App;
+const mapStateToProps = state => {
+  console.log('state', state);
+
+  return {gameStarted: state.gameStarted };
+}
+
+const comonentConnector = connect(mapStateToProps);
+
+export default comonentConnector(App);
--- src/index.js
+++ src/index.js
@@ -1,12 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { createStore } from 'redux';
+import { Provider } from 'react-redux';
 import App from './components/App';
 import rootReducer from './reducers';
 import './index.css'
-
-
-
 
 const store = createStore(rootReducer);
 
@@ -15,5 +13,10 @@ console.log('store', store);
 
 store.subscribe(() => console.log('store.getState()', store.getState()));
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+
+  </Provider>,
+document.getElementById('root'));
 
