3330a466dbd9f7e4e747b3ca688268510d34b091
Laurie Roy <laurieroy.dev@gmail.com>
2019-04-25 19:55:18 -0700

    Add UI, using this.props.dispatch

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,33 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import { startGame, cancelGame } from '../actions/settings';
+// import  {startGame} from '../actions/settings';
 
 class App extends Component {
-  render() {
+  startGame = () => {this.props.dispatch(startGame())}
+  cancelGame = () => {this.props.dispatch(cancelGame())}
+
+    render() {
     console.log('this', this);
     return (
-      <div>React App</div>
+      <div>
+        <h2>Evens or Odds</h2>
+      {
+        this.props.gameStarted ? (
+          <div>
+            <h3>The game is on!</h3>
+            <br />
+            <button onClick={this.cancelGame}>Cancel Game</button>
+          </div>
+        ) : (
+          <div>
+            <h3>A new game awaits</h3>
+            <br />
+            <button onClick={this.startGame}>Start Game</button>
+          </div>
+        )
+      }
+      </div>
     );
   }
 }
