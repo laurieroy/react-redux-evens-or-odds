55d5c09ab4998079b80d102c60a852de9e634ea0
Laurie Roy <laurieroy.dev@gmail.com>
2019-04-25 18:12:12 -0700

    initial commit

--- src/index.js
+++ src/index.js
@@ -1,7 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createStore } from 'redux';
 import App from './components/App';
+import rootReducer from './reducers';
 import './index.css'
+
+
+
+
+const store = createStore(rootReducer);
+
+console.log('store.getState()', store.getState());
+console.log('store', store);
+
+store.subscribe(() => console.log('store.getState()', store.getState()));
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
