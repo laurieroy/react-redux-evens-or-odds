dd58c737734b786e0c59a9036577c1f0fdcef4ac
Laurie Roy <laurieroy.dev@gmail.com>
2019-04-27 16:07:47 -0700

    Add Card feature

--- src/components/App.js
+++ src/components/App.js
@@ -5,6 +5,7 @@ import { fetchNewDeck } from '../actions/deck';
 import fetchStates from '../reducers/fetchStates';
 import Instructions from './Instructions';
 import DrawCard from './DrawCard';
+import Card from './Card';
 
 class App extends Component {
   startGame = () => {
@@ -32,6 +33,8 @@ class App extends Component {
             <h3>The game is on!</h3>
             <br />
             <DrawCard />
+            <hr />
+            <Card />
             <hr />
             <button onClick={this.props.cancelGame}>Cancel Game</button>
           </div>
